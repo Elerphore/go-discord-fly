01f935e0a5c29171e2956bbfd25bf9a2487d4c2f
Eremey (Elerphore) Malinovsky <simon6041@gmail.com>
2022-02-27 23:18:05 +0500

    *

--- fly.go
+++ fly.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world")
+
+	client := &http.Client{}
+
+	requestBody, err := json.Marshal(map[string]string{
+		"content": "Again.",
+	})
+
+	if err != nil {
+		log.Fatal("error")
+	}
+
+	const webhookToken = "https://discord.com/api/webhooks/947040904034779156/zSAT0yoEUsgK02tjNHAuaPKtsf_UEoXSr3B1Mhv7qINlSKCQqevCpqXVsBOuNMT30wYE"
+	req, err := http.NewRequest("POST", webhookToken, bytes.NewBuffer(requestBody))
+	req.Header.Add("Content-Type", "application/json")
+
+	if err != nil {
+		log.Fatal("error")
+	}
+
+	client.Do(req)
 }
