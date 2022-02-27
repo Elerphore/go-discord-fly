26d7438b63ad00ccf86988cc58645176ff7defba
Eremey (Elerphore) Malinovsky <simon6041@gmail.com>
2022-02-27 23:25:09 +0500

    *

--- fly.go
+++ fly.go
@@ -19,7 +19,7 @@ func main() {
 		log.Fatal("error")
 	}
 
-	const webhookToken = "https://discord.com/api/webhooks/947040904034779156/zSAT0yoEUsgK02tjNHAuaPKtsf_UEoXSr3B1Mhv7qINlSKCQqevCpqXVsBOuNMT30wYE"
+	const webhookToken = ""
 	req, err := http.NewRequest("POST", webhookToken, bytes.NewBuffer(requestBody))
 	req.Header.Add("Content-Type", "application/json")
 
