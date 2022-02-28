dc502ce8764daaa1967ddf4c47ad04d2d95701a5
Eremey (Elerphore) Malinovsky <simon6041@gmail.com>
2022-02-28 07:11:07 +0500

    Components, Component field struct

--- fly.go
+++ fly.go
@@ -9,26 +9,46 @@ import (
 
 func main() {
 
+	var comps [1]ComponentField
+
+	comps[0] = ComponentField{
+		Type:     2,
+		Label:    "Click me!",
+		Style:    1,
+		CustomId: "click_one",
+	}
+
+	componentOne :=
+		Component{
+			Type:       1,
+			Components: comps,
+		}
+
+	var compss [1]Component
+
+	compss[0] = componentOne
+
 	msg := WebHookMessage{
-		Content: "There's some structs I've used here.",
-		Tts:     true,
+		Content:    "Lol, sorry.",
+		Tts:        false,
+		Username:   "Some random name",
+		AvatarUrl:  "https://i.pinimg.com/originals/cd/bf/5f/cdbf5f93123170b215d1d81980d8a1ca.jpg",
+		Components: compss,
 	}
 
 	client := &http.Client{}
 
 	requestBody, err := json.Marshal(msg)
 
-	log.Println(requestBody)
-
 	if err != nil {
-		log.Fatal("error")
+		log.Fatal(err)
 	}
 
 	req, err := http.NewRequest("POST", webhookToken, bytes.NewBuffer(requestBody))
 	req.Header.Add("Content-Type", "application/json")
 
 	if err != nil {
-		log.Fatal("error")
+		log.Fatal(err)
 	}
 
 	client.Do(req)
--- webHookMessageStruct.go
+++ webHookMessageStruct.go
@@ -1,6 +1,21 @@
 package main
 
+type Component struct {
+	Type       int               `json:"type"`
+	Components [1]ComponentField `json:"components"`
+}
+
+type ComponentField struct {
+	Type     int    `json:"type"`
+	Label    string `json:"label"`
+	Style    int    `json:"style"`
+	CustomId string `json:"custom_id"`
+}
+
 type WebHookMessage struct {
-	Content string `json:"content"`
-	Tts     bool   `json:"tts"`
+	Content    string       `json:"content"`
+	Tts        bool         `json:"tts"`
+	Username   string       `json:"username"`
+	AvatarUrl  string       `json:"avatar_url"`
+	Components [1]Component `json:"components"`
 }
